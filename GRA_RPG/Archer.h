4219023815cdd57dfa72a1e4589284d29d33d4c1
Kornel Krzywacki <kornel1997k@gmail.com>
2020-06-12 00:25:35 +0200

    hgjh

--- GRA_RPG/Archer.h
+++ GRA_RPG/Archer.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Champion.h"
 
+//kkkk
 class Archer : public Champion {
 public:
 	Archer(const unsigned id, const std::string n, unsigned int l, unsigned int e, double m, int he, int st, int de, int ma, int def, int lu) :
